432ec5ff3db0b9bdf546c41facedd2048ec7acb2
jennytemple <jennytemple@gmail.com>
2017-10-24 17:15:59 -0700

    adding insight on sequence of campaigns

--- src/investigate_user_behavior.py
+++ src/investigate_user_behavior.py
@@ -26,6 +26,59 @@ def get_campaign_freq(df_levels):
     return df_levels_freq
 
 
+def get_drop_off_by_num_levels(df_users):
+    '''
+    if percent_balk at Level 12 = .39, 39% of users who played 12 levels did not play more than 12 levels
+    '''
+    level_freq = df_users['Levels Completed'].value_counts()
+    level_freq = pd.DataFrame(level_freq)
+    level_freq['num_levels_completed'] = level_freq.index
+    level_freq = level_freq.sort_values('num_levels_completed')
+    level_freq = level_freq.rename(
+        columns={'Levels Completed': 'final_num_users'})
+    level_freq['total_users'] = level_freq['final_num_users'].sum()
+    level_freq['cumulative_balked'] = level_freq['final_num_users'].cumsum()
+    level_freq['users_remaining'] = level_freq['total_users'] - \
+        level_freq['cumulative_balked']
+    level_freq['users_previous'] = level_freq['users_remaining'].shift(1)
+    level_freq['users_previous'] = level_freq['users_previous'].fillna(
+        level_freq['total_users'])
+    level_freq['percent_retained'] = level_freq['users_remaining'] / \
+        level_freq['users_previous']
+    level_freq['percent_balk'] = 1.0 - level_freq['percent_retained']
+
+    # plot over time
+    level_freq.plot.line('num_levels_completed', 'percent_balk')
+    plt.show()
+
+    # order by greatest % balk
+    level_freq[['num_levels_completed', 'percent_balk']
+               ].sort_values('percent_balk', ascending=False)
+    return level_freq
+
+
+def unpaid_users_over_level(df_users, df_levels, n=12):
+    df_unpaid = df_users[df_users['Paid Subscription'] == False]
+    df_unpaid = df_unpaid['Id'][df_unpaid['Levels Completed'] > n]
+    levels_over_n_unpaid = df_levels[df_levels['User Id'].isin(df_unpaid)]
+    return levels_over_n_unpaid
+
+
+def unpaid_level_freq(df_unpaid):
+    unpaid_level_freq = get_campaign_freq(df_unpaid)
+    print unpaid_level_freq[unpaid_level_freq['last_campaign_started'] == 'dungeon']
+    print unpaid_level_freq[unpaid_level_freq['last_campaign_started'] == 'forest']
+    print unpaid_level_freq[unpaid_level_freq['last_campaign_started'] == 'desert']
+    print unpaid_level_freq[unpaid_level_freq['last_campaign_started'] == 'mountain']
+    print unpaid_level_freq[unpaid_level_freq['last_campaign_started'] == 'glacier']
+    # results: forest starts at 13, but more dungeon is mixed in
+    # desert starts solidly at 60 but other levels mixed in still
+    # mountain is less clear 132,162, 165,
+    # glacier has one level at 137, played by 193 people, but then no more until 345, and still not concentratetd
+
+    # data to use: first dungeon set for
+
+
 if __name__ == '__main__':
     sample_path = '../../data/sample/'
     august_path = '../../data/august/'
@@ -41,3 +94,9 @@ if __name__ == '__main__':
     user_df = df_users[df_users['Id'] == user]
     user_levels = df_levels[df_levels['User Id'] == user]
     user_events = df_events[df_events['User Id'] == user]
+
+    level_freq = get_drop_off_by_num_levels(df_users)
+
+    df_unpaid_over_12_levels = unpaid_users_over_level(
+        df_users, df_levels, n=12)
+    df_freq_unpaid_over_12_levels = get_campaign_freq(df_unpaid_over_12_levels)
