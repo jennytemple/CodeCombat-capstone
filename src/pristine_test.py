90427ac678b751fc8618e233809ed1f769746f08
jennytemple <jennytemple@gmail.com>
2017-10-29 21:18:56 -0700

    adding logistic regression, but using different scaler

--- src/pristine_test.py
+++ src/pristine_test.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler, scale
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import f1_score, accuracy_score, precision_score, recall_score, confusion_matrix, roc_curve, roc_auc_score
 from sklearn.ensemble import RandomForestClassifier
@@ -31,23 +31,41 @@ if __name__ == '__main__':
         df, y = fix_target_and_drop_target_fields(df, 'Levels Completed')
         y, name = categorize_by_level_num(y, 2, threshold=level_predict)
         X = df.values
-        scaler = final_model = pickle.load(open('models/feature_scaler', "rb"))
-        X_scaled = scaler.transform(X)
-        # read model
-        model_to_use = 'models/model_predict_' + \
+
+        # real Random Forest model
+        rf_model_to_use = 'models/model_predict_' + \
             str(level_predict) + '_rf_grid.p'
-        final_rf_model = pickle.load(open(model_to_use, "rb"))
+        final_rf_model = pickle.load(open(rf_model_to_use, "rb"))
 
-        y_predict_rf = final_rf_model.predict(X)
-        proba_rf = final_rf_model.predict_proba(X)
+        y_predict = final_rf_model.predict(X)
+        proba = final_rf_model.predict_proba(X)
 
         print"\n\t******************************"
-        print"\nUsing this model: {} to make prediction on this data: {}".format(model_to_use, csv_name)
+        print"\nUsing this model: {} to make prediction on this data: {}".format(rf_model_to_use, csv_name)
         print"\n\t******************************"
-        print "\nThe results of the model on pristine test data:"
-        print_scores(y, y_predict_rf)
+        print "\nThe results of the RANDOM FOREST model on pristine test data:"
+        print_scores(y, y_predict)
         # fpr, tpr, thresholds = roc_curve(y_test, proba[:, 1])
-        auc = roc_auc_score(y, proba_rf[:, 1])
+        auc = roc_auc_score(y, proba[:, 1])
+        print "\nThe area under the roc curve is: {}\n".format(auc)
+
+        # real Lasso Logistic Regression model
+        # scaler = pickle.load(open('models/feature_scaler.p', "rb"))
+        # X_scaled = scaler.transform(X) #error: "operands could not be broadcast together"
+        X_scaled = scale(X)
+        lrl_model_to_use = 'models/model_predict_' + \
+            str(level_predict) + '_lrl.p'
+        final_lrl_model = pickle.load(open(lrl_model_to_use, "rb"))
+        y_predict = final_lrl_model.predict(X_scaled)
+        proba = final_lrl_model.predict_proba(X_scaled)
+
+        print"\n\t******************************"
+        print"\nUsing this model: {} to make prediction on this data: {}".format(lrl_model_to_use, csv_name)
+        print"\n\t******************************"
+        print "\nThe results of the LOGISTIC REGRESSION LASSO model on pristine test data:"
+        print_scores(y, y_predict)
+        # fpr, tpr, thresholds = roc_curve(y_test, proba[:, 1])
+        auc = roc_auc_score(y, proba[:, 1])
         print "\nThe area under the roc curve is: {}\n".format(auc)
 
         print "\nThe results of the model on baseline (predicting the majority class):"
